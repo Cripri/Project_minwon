35cb36160295aef0ee615f41b1bbe4c2a0fcf2f3
Si7C-08 <Si7C-08@i7B-32>
2025-07-11 17:11:18 +0900

    아이콘버튼 클래스 추가

--- mainsouce/src/gui/mainframe/components/PaginationPanel.java
+++ mainsouce/src/gui/mainframe/components/PaginationPanel.java
@@ -2,12 +2,9 @@ package gui.mainframe.components;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 
 public class PaginationPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
-	private final JButton prevBtn;
-    private final JButton nextBtn;
     private final JLabel pageLabel;
     private int currentPage = 0;
     private final int totalPages;
@@ -23,35 +20,12 @@ public class PaginationPanel extends JPanel {
         setOpaque(false);
         setLayout(new FlowLayout(FlowLayout.CENTER));
 
-        ImageIcon prevIcon = new ImageIcon("./IconImage/이전페이지.png");
-        ImageIcon nextIcon = new ImageIcon("./IconImage/다음페이지.png");
-        Image prevScaled = prevIcon.getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH);
-        Image nextScaled = nextIcon.getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH);
-
-        prevBtn = new JButton("이전", new ImageIcon(prevScaled));
-        nextBtn = new JButton("다음", new ImageIcon(nextScaled));
+        IconButton prevBtn = new IconButton("이전 페이지", "./IconImage/이전페이지.png");
+        IconButton nextBtn = new IconButton("다음 페이지", "./IconImage/다음페이지.png");
         pageLabel = new JLabel();
 
         prevBtn.setHorizontalTextPosition(SwingConstants.RIGHT);
         nextBtn.setHorizontalTextPosition(SwingConstants.LEFT);
-
-        for (JButton btn : new JButton[]{prevBtn, nextBtn}) {
-            btn.setOpaque(false);
-            btn.setContentAreaFilled(false);
-            btn.setBorderPainted(false);
-            btn.setFont(new Font("맑은 고딕", Font.PLAIN, 14));
-            btn.addMouseListener(new MouseAdapter() {
-                @Override
-                public void mouseEntered(MouseEvent e) {
-                    btn.setCursor(new Cursor(Cursor.HAND_CURSOR));
-                }
-
-                @Override
-                public void mouseExited(MouseEvent e) {
-                    btn.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
-                }
-            });
-        }
 
         prevBtn.addActionListener(e -> {
             if (currentPage > 0) {
