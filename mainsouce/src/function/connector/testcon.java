ad6a36cd373ab03eedfd21c046cfca5c8e7de0d3
Cripre <farenhite97@naver.com>
2025-07-14 15:13:20 +0900

    testcon upload

--- mainsouce/src/function/connector/testcon.java
+++ mainsouce/src/function/connector/testcon.java
@@ -1,54 +1,30 @@
 package function.connector;
 
 import function.pdfwriter.PDFWriter;
-import gui.mainframe.MainFrameState;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+
 
 public class testcon {
-    public static void main(String[] args) {
-        MainFrameState.civil.start();
+    public static void main(String[] args){
+        Civil_Connector con = new Civil_Connector();
+        con.start();
 
-        QueryRequest<Members> mem = new QueryRequest<>(
-                "SELECT * FROM members WHERE MEMBER_ID like ?",
-                "아이디 가져오기",
-                "Members",
-                "joyj",
-                QueryType.SELECT,
-                Members.class
+        QueryRequest<Simple_doc> doc = new QueryRequest<>(
+                "select * from simple_doc where simple_doc_code like ?",
+                2,
+                Simple_doc.class,
+                con
         );
 
-//        QueryRequest<Simple_doc> target = new QueryRequest<>(
-//                "select * from simple_doc",
-//                "findallSimple_doc",
-//                "Simple_doc",
-//                null,
-//                QueryType.SELECT,
-//                Simple_doc.class
-//        );
 
-        try {
-            //query.putQuery(target);
-            MainFrameState.civil.putQuery(mem);
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        Simple_doc data = doc.getSingleResult();
 
-        Members mem1 = mem.getSingleResult();
-        List<Members> member = mem.getResultList();
-        //List<Simple_doc> data = target.getResultList();
-//        for(Simple_doc s : data){
-//            System.out.println(s);
-//        }
-//        for(Members m : member){
-//            System.out.println(m);
-//        }
-        System.out.println(mem1.getMember_id());
+        System.out.println(data);
 
-
-
-        //new PDFWriter(1,"123456-1234567",query);
+        new PDFWriter(3,"123456-1234567",con);
 
     }
 }
