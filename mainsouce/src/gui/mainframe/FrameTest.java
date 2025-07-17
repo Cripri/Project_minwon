d86002fdab24d3b64d35fa45c734ad502a10b442
Si7C-08 <Si7C-08@i7B-32>
2025-07-17 17:41:00 +0900

    다시 푸시 (비회원 로그인 패널 추가, 아이콘 이미지 추가, 엔터키로 로그인 등등)

--- mainsouce/src/gui/mainframe/FrameTest.java
+++ mainsouce/src/gui/mainframe/FrameTest.java
@@ -5,21 +5,21 @@ import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
-import gui.phs.WriteContent;
-import gui.phs.AfterLoginPanel;
+//import gui.phs.AfterLoginPanel;
 import gui.phs.CivilComplaintDetailPanel;
 import gui.phs.ComplaintAnswerListPanel;
 import gui.phs.DepartmentChangeRequestDetailPanel;
 import gui.phs.DepartmentChangeRequestPanel;
 import gui.phs.FirstPage;
 import gui.phs.RrnApplicationPanel;
+import gui.phs.WriteContent;
 import gui.phs.ManagerMenu.ManagerMenuPanel;
 
 public class FrameTest extends JFrame {
 	private static final long serialVersionUID = 1L;
 
 	public FrameTest() {
-		setTitle("Login UI");
+		setTitle("Project_minwon");
 		setSize(1600, 900);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocationRelativeTo(null); // 화면 중앙
@@ -34,8 +34,9 @@ public class FrameTest extends JFrame {
 		SignUpPanel signUp = new SignUpPanel();
 		UserInfoEditPanel userInfoEdit = new UserInfoEditPanel();
 		SinmungoDetailPanel pppp = new SinmungoDetailPanel(1, "마이페이지");
+		GuestLoginPanel guestLogin = new GuestLoginPanel();
 		
-		AfterLoginPanel afterLoginPanel = new AfterLoginPanel();
+//		AfterLoginPanel afterLoginPanel = new AfterLoginPanel();
 		CivilComplaintDetailPanel civilComplaintDetailPanel = new CivilComplaintDetailPanel();
 		ComplaintAnswerListPanel complaintAnswerListPanel = new ComplaintAnswerListPanel();
 		DepartmentChangeRequestPanel departmentChangeRequestPanel = new DepartmentChangeRequestPanel();
@@ -48,17 +49,6 @@ public class FrameTest extends JFrame {
 		add(ft, BorderLayout.NORTH);
 
 		CardLayoutPanel cardPage = new CardLayoutPanel();
-		cardPage.add("userInfoEdit", userInfoEdit);
-		
-//		cardPage.add("CivilComplaintDetailPanel", civilComplaintDetailPanel);
-//		cardPage.add("AfterLoginPanel", afterLoginPanel);
-//		cardPage.add("ComplaintAnswerListPanel", complaintAnswerListPanel); // 주민번호 지워야함 
-//		cardPage.add("DepartmentChangeRequestPanel", departmentChangeRequestPanel); // 완성 창 제목 넣어주면 좋음
-//		cardPage.add("DepartmentChangeRequestDetailPanel", departmentChangeRequestDetailPanel); 주민번호 지워야함
-//		cardPage.add("RrnApplicationPanel", rrnApplicationPanel); //선택창 더 만들어야 함
-//		cardPage.add("WriteContent", writeContent); // 작성완료 버튼 지우기 
-//		cardPage.add("ManagerMenuPanel", managerMenuPanel); // 위에 제목 만드
-		
 		
 		cardPage.add("firstPage", firstPage);
 		cardPage.add("login", login);
@@ -67,10 +57,18 @@ public class FrameTest extends JFrame {
 		cardPage.add("sinmungoList", sinmungoList);
 		cardPage.add("employeeMain", employeeMain);
 		cardPage.add("signUp", signUp);
+		cardPage.add("userInfoEdit", userInfoEdit);
+		cardPage.add("guestLogin", guestLogin);
 		
-
+		cardPage.add("civilComplaintDetailPanel", civilComplaintDetailPanel);
+//		cardPage.add("afterLoginPanel", afterLoginPanel);
+		cardPage.add("complaintAnswerListPanel", complaintAnswerListPanel); // 주민번호 지워야함 
+		cardPage.add("departmentChangeRequestPanel", departmentChangeRequestPanel); // 완성 창 제목 넣어주면 좋음
+		cardPage.add("departmentChangeRequestDetailPanel", departmentChangeRequestDetailPanel); // 주민번호 지워야함
+		cardPage.add("rrnApplicationPanel", rrnApplicationPanel); //선택창 더 만들어야 함
+		cardPage.add("writeContent", writeContent); // 작성완료 버튼 지우기 
+		cardPage.add("managerMenuPanel", managerMenuPanel); // 위에 제목 만드
 		
-
 		MainFrameState.history.clear();
 		MainFrameState.future.clear();
 		add(cardPage, BorderLayout.CENTER);
